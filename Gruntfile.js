7f6a4ef56a9390785b16682b4adfb8eb2f637e60
Jose Eduardo Biasioli <biasiolije@gmail.com>
2015-05-07 10:55:08 -0300

    Teste do Travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,41 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        includePaths: ['bower_components/foundation/scss']
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          sourceMap: true,
-        },
-        files: {
-          'css/unesp-foundation.css': 'scss/unesp-foundation.scss',
-          'css/app.css': 'scss/app.scss'
-        }
-      }
-    },
+		sass: {
+			options: {
+				includePaths: ['bower_components/foundation/scss']
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed',
+					sourceMap: true,
+				},
+				files: {
+					'css/unesp-foundation.css': 'scss/unesp-foundation.scss',
+					'css/central.css': 'scss/central.scss'
+				}
+			}
+		},
 
-    watch: {
-      grunt: {
-        options: {
-          reload: true
-        },
-        files: ['Gruntfile.js']
-      },
+		watch: {
+			grunt: {
+				options: {
+					reload: true
+				},
+				files: ['Gruntfile.js']
+			},
 
-      sass: {
-        files: 'scss/**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+			sass: {
+				files: 'scss/**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+	grunt.registerTask('build', ['sass']);
+	grunt.registerTask('default', ['build','watch']);
 }
